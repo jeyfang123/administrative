90f4c9d43dd3d92cf3a64b67550471dfb8330c4c
jeyfang <fangcheng@pimpin.cn>
2017-05-14 21:03:02 +0800

    add process

--- web/app/controller/easier/process.controller.php
+++ web/app/controller/easier/process.controller.php
@@ -6,8 +6,51 @@
  * Time: 16:06
  */
 class ProcessController extends Controller{
+
     function index($req){
         $type = $req->param('type');
+        $department = Box::getObject('department','model','admin')->getAllDepartment();
+        $proTypes = Box::getObject('process','model','admin')->getProType();
+        if($type == 'person'){
+            $proType = $proTypes['per'];
+        }
+        else if($type == 'enter'){
+            $proType = $proTypes['enter'];
+        }
+        else{
+            $proType = array_merge($proTypes['per'],$proTypes['enter']);
+        }
+        $this->_twig->assign('data',['proType'=>$proType,'department'=>$department]);
         return $this->viewTpl('process.html');
+    }
+
+    function searchProcess($req){
+        $type = $req->param('type');
+        $index = $req->param('index');
+        $keyWords = $req->param('keyWords','');
+
+        $page = $req->param('page',1);
+        $pageSize = $req-param('pageSize',10);
+        $map = " pro_name like ? ";
+        $processObj = Box::getObject('process','model','easier');
+        if($type == '-1'){
+            $res = $processObj->searchProcess($page,$pageSize,$map,"%{$keyWords}%");
+        }
+        else if($type == 'depart'){
+            $map .= " and pro_id in (select DISTINCT(pro_id) from process_node where role = ?) ";
+            $res = $processObj->searchProcess($page,$pageSize,$map,"%{$keyWords}%",$index);
+        }
+        else if($type == 'type'){
+            $map .= " and pro_type = ? ";
+            $res = $processObj->searchProcess($page,$pageSize,$map,"%{$keyWords}%",$index);
+        }
+        if($res == false){
+            return $this->returnJson(['code'=>CODE_ERROR,'msg'=>'查询失败']);
+        }
+        else if($res == null){
+            return $this->returnJson(['code'=>CODE_SUCCESS,'data'=>[],'count'=>0]);
+        }
+        return $this->returnJson(['code'=>CODE_SUCCESS,'data'=>$res['data'],'count'=>$res['count']]);
+
     }
 }
---
+++ web/app/model/easier/process.controller.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jey
+ * Date: 2017/5/14
+ * Time: 20:19
+ */
+class ProcessModel{
+    private $_db;
+    function __construct()
+    {
+        $this->_db = DB::getInstance();
+    }
+
+    function searchProcessHasDepart($role){
+        $sql = "select DISTINCT(pro_id) from process_node 
+                    where \"role\" = ? ";
+        $res = $this->_db->GetAll($sql,[$role]);
+        $sql = "pro_id in(
+select DISTINCT(pro_id) from process_node 
+where \"role\" = '7588a16a-d216-43e7-a97f-bbd2b46e6c30')";
+    }
+
+    function searchProcess($page=1,$pageSize=10,$map = null){
+        $offset = ($page-1)*$pageSize;
+        if($map == null){
+            $sql = "select pro_name,pro_id,heatpoint from ".DB::TB_PROCESS." LEFT JOIN
+                    (select lefttab.*, case when righttab.per_type = '-1' then lefttab.enter_type 
+                        when righttab.enter_type = '-1' then lefttab.per_type end as pro_type 
+                    from ".DB::TB_PRO_DETAIL." lefttab LEFT JOIN ".DB::TB_PRO_DETAIL." righttab on lefttab.pop_id = righttab.pop_id) pro_detail
+                on pro_id = pop_id
+                LEFT JOIN ".DB::TB_TYPE." on pro_type = type_id where 1=1 offset {$offset} limit {$pageSize}";
+            $res = $this->_db->GetAll($sql);
+            $count = "select count(*) count from ".DB::TB_PROCESS." LEFT JOIN
+                    (select lefttab.*, case when righttab.per_type = '-1' then lefttab.enter_type 
+                        when righttab.enter_type = '-1' then lefttab.per_type end as pro_type 
+                    from ".DB::TB_PRO_DETAIL." lefttab LEFT JOIN ".DB::TB_PRO_DETAIL." righttab on lefttab.pop_id = righttab.pop_id) pro_detail
+                on pro_id = pop_id
+                LEFT JOIN ".DB::TB_TYPE." on pro_type = type_id where 1=1 ";
+            $total = $this->_db->GetOne($count);
+        }
+        else{
+            $params = array_slice(func_get_args(),3);
+            $sql = "select pro_name,pro_id,heatpoint from ".DB::TB_PROCESS." LEFT JOIN
+                    (select lefttab.*, case when righttab.per_type = '-1' then lefttab.enter_type 
+                        when righttab.enter_type = '-1' then lefttab.per_type end as pro_type 
+                    from ".DB::TB_PRO_DETAIL." lefttab LEFT JOIN ".DB::TB_PRO_DETAIL." righttab on lefttab.pop_id = righttab.pop_id) pro_detail
+                on pro_id = pop_id
+                LEFT JOIN ".DB::TB_TYPE." on pro_type = type_id where 1=1 and {map} offset {$offset} limit {$pageSize}";
+            $res = $this->_db->GetAll($sql,$params);
+            $count = "select count(*) count from ".DB::TB_PROCESS." LEFT JOIN
+                    (select lefttab.*, case when righttab.per_type = '-1' then lefttab.enter_type 
+                        when righttab.enter_type = '-1' then lefttab.per_type end as pro_type 
+                    from ".DB::TB_PRO_DETAIL." lefttab LEFT JOIN ".DB::TB_PRO_DETAIL." righttab on lefttab.pop_id = righttab.pop_id) pro_detail
+                on pro_id = pop_id
+                LEFT JOIN ".DB::TB_TYPE." on pro_type = type_id where 1=1 and {$map}";
+            $total = $this->_db->GetOne($count,$params);
+        }
+        return DB::returnModelRes(['data'=>$res,'count'=>$total])[0];
+    }
+}
